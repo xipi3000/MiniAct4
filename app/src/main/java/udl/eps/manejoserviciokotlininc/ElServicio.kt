8367af67fca0bd1a3f9ca3b750bff71d41c544dc
Pol <polescola3000@gmail.com>
2023-04-19 09:55:59 +0200

    A,B

--- app/src/main/java/udl/eps/manejoserviciokotlininc/ElServicio.kt
+++ app/src/main/java/udl/eps/manejoserviciokotlininc/ElServicio.kt
@@ -8,22 +8,9 @@ import android.widget.Toast
 
 class ElServicio: Service() {
 
-    private var player: MediaPlayer? = null
-
+    private var playerSound: MediaPlayer? = null
+    private var playerMusic: MediaPlayer? = null
     override fun onBind(p0: Intent?): IBinder? {
-        val type = p0?.getStringExtra("type")
-        if(type=="sound"){
-            player = MediaPlayer.create(this, R.raw.train);
-        }
-        else{
-            val url = "https://www.youtube.com/watch?v=V1l6kxQNq54&pp=ygUTbXVyZWF4IHBlcmZlY3QgZ2lybA%3D%3D"
-            player = MediaPlayer().apply {
-                setDataSource(url)
-                prepare() // might take long! (for buffering, etc)
-                start()
-            }
-            //player = MediaPlayer.create(this, R.raw.PerfectGirl);
-        }
 
         return null
     }
@@ -31,21 +18,31 @@ class ElServicio: Service() {
     override fun onCreate() {
         super.onCreate()
         Toast.makeText(this, R.string.creaserv, Toast.LENGTH_LONG).show()
-
-        player!!.isLooping = true
+        playerSound = MediaPlayer.create(this,R.raw.train)
+        playerSound!!.isLooping = true
+        playerMusic = MediaPlayer.create(this,R.raw.PerfectGirl_Mureaux)
+        playerMusic!!.isLooping = true
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         super.onStartCommand(intent, flags, startId)
         Toast.makeText(this, R.string.iniserv, Toast.LENGTH_LONG).show()
-
+        if(intent?.getStringExtra("source")=="sound"){
+            playerSound?.start()
+        }
+        else {
+            playerMusic?.start()
+        }
         return startId
     }
+
 
     override fun onDestroy() {
         super.onDestroy()
         Toast.makeText(this, R.string.finaserv, Toast.LENGTH_LONG).show()
-        player?.release()
-
+        if(playerSound!!.isPlaying) playerSound?.stop()
+        if(playerMusic!!.isPlaying) playerMusic?.stop()
+        playerSound?.release()
+        playerMusic?.release()
     }
 }
